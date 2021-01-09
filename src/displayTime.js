dfab1dbafac3dd40ff215d0dafeae7290f66f87f
Victoria <victoria.fischer@cix.csi.cuny.edu>
2021-01-08 20:15:35 -0500

    added 5 min break feature

--- src/displayTime.js
+++ src/displayTime.js
@@ -10,11 +10,14 @@ class DisplayTime extends React.Component{
         this.stopTime = this.stopTime.bind(this);
         this.handleClick3 = this.handleClick3.bind(this);
         this.handleClick4 = this.handleClick4.bind(this);
+        this.startBreak = this.startBreak.bind(this);
       }
 
     state = {
         minutes: 25,
-        seconds: 0
+        seconds: 0,
+        breakMin: 5,
+        breakSec: 0
       }
 
     startTime() {
@@ -40,9 +43,42 @@ class DisplayTime extends React.Component{
         }, 1000)
     }
 
+    startBreak(){
+        this.stopTime()
+        this.myInterval = setInterval(() => {
+            this.setState(prevState => {
+                if(prevState.breakSec !== 0){
+                    return {
+                        breakSec: prevState.breakSec - 1
+                    }
+                } else if (prevState.breakSec === 0 && prevState.breakMin === 0){
+                    this.stopBreak();
+                    this.startTime()
+                    return {
+                        breakMin: 5,
+                        breakSec: 0
+                    }
+                }
+                else{
+                    return {
+                        breakMin: prevState.breakMin - 1,
+                        breakSec: 59
+                    }
+                }
+            })
+        }, 1000)
+    }
+    
+    stopBreak() {
+        clearInterval(this.myInterval)
+    }
+    
+
+
     stopTime() {
         clearInterval(this.myInterval)
     }
+    
 
     handleClick1() {
         this.setState(state => {
@@ -103,7 +139,7 @@ class DisplayTime extends React.Component{
       }
 
     render(){
-        const { minutes, seconds } = this.state
+        const { minutes, seconds, breakMin, breakSec, break10Min } = this.state
         return (
             <div>
                 <h1>Pomodoro Clock</h1>
@@ -118,6 +154,10 @@ class DisplayTime extends React.Component{
                     <button onClick={this.handleClick3}>Subtract 1 Min</button>
                     <button onClick={this.handleClick4}>Subtract 1 Sec</button>
                 </div>
+
+                <button onClick={this.startBreak}>Take 5min Break</button>
+                <p>Break: {breakMin}:{breakSec < 10 ? `0${ breakSec }` : breakSec }</p>
+               
             </div>
             )
     }
